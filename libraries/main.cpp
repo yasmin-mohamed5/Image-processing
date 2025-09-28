c4b4e49b3c5be50ddf8849b715b1f123045c93a9
yasmin-mohamed5 <mony.mony005@gmail.com>
2025-09-28 12:35:30 +0300

    add menu

--- libraries/main.cpp
+++ libraries/main.cpp
@@ -1,4 +1,3 @@
-#include<iostream>
 #include"Image_Class.h"
 using namespace std;
 void invert(Image &image){
@@ -9,13 +8,15 @@ void invert(Image &image){
             image(i,j,2) = 255-image(i,j,2);
         }
     }
+    cout<<"The filter has been applied successfully!\n";
 }
 
 void rotate(Image &image){
     cout<<"please enter the rotation degree : ";
+    cout<<"1-90 / 2-180 / 3-270\n";
     int rotarion;
     cin>>rotarion;
-    if(rotarion == 90){
+    if(rotarion == 1){
         Image img2(image.height,image.width);
         for(int i=img2.width-1;i>=0;i--){
             for(int j=0;j<img2.height;j++){
@@ -25,7 +26,7 @@ void rotate(Image &image){
             }
         }
         image=img2;
-    }else if(rotarion == 180){
+    }else if(rotarion == 2){
         Image img2(image.width,image.height);
         for(int i=img2.width-1;i>=0;i--){
             for(int j=img2.height-1;j>=0;j--){
@@ -35,7 +36,7 @@ void rotate(Image &image){
             }
         }
         image=img2;
-    }else if(rotarion == 270){
+    }else if(rotarion == 3){
         Image img2(image.height,image.width);
         for(int i=0;i<img2.width;i++){
             for(int j=img2.height-1;j>=0;j--){
@@ -46,25 +47,102 @@ void rotate(Image &image){
         }
         image = img2;
     }
-
+    cout<<"The filter has been applied successfully!\n";
 }
 
 int main(){
-    string file_name, newfilename,filter;
+    string file_name, newfilename;
     cout<<"please enter your file name : ";
-    getline(cin,file_name);
-    Image image(file_name);
-    cout<<"avilable filters : invert / rotate\n";
-    cout<<"please enter your filter : ";
-    cin>>filter;
-    if(filter == "invert"){
-        invert(image);
-    }else if(filter == "rotate"){
-        rotate(image);
+    cin>>file_name;
+    Image image;
+    try {
+        bool loaded = image.loadNewImage(file_name);  
+        if (loaded) {
+            Image img(file_name);
+            image=img;
+            cout << "Image loaded successfully!\n";
+        }
+    } catch (const invalid_argument& e) {
+        cout << "Error: " << e.what() << '\n';
     }
-    cout<<"please enter your new file name : ";
-    getline(cin,newfilename);
-    image.saveImage(newfilename);
+    while(true){
+        cout<< "please enter a number from the following choices:\n";
+        cout<<"1-Load a new image / 2-invert / 3-rotate / 4-save / 5-Exit\n";
+        int choice;
+        cin>>choice;
+        if(choice == 1){
+            cout<<"do you want to save the current one before loading a new one?\n";
+            cout<<"1-yes / any other number is no\n";
+            int y;
+            cin>>y;
+            if(y==1){
+                cout<<"do you want to 1-save on the same file or 2-change file name\n";
+                int x;
+                cin>>x;
+                if(x==1){
+                    image.saveImage(file_name);
+                }else if(x == 2){
+                    cout<<"please enter the new file name : ";
+                    cin>>newfilename;
+                    try {
+                        bool loaded = image.loadNewImage(newfilename);  
+                        if (loaded) {
+                            image.saveImage(newfilename);
+                            cout<<"file saved successfully!\n";
+                        }
+                    } catch (const invalid_argument& e) {
+                        cout << "Error: " << e.what() << '\n';
+                    }
+                }else{
+                    cout<<"you enter the wrong number\n";
+                }
+            }else{
+                cout<<"please enter the new image : ";
+                cin>>file_name;
+                try {
+                    bool loaded = image.loadNewImage(file_name);  
+                    if (loaded) {
+                        Image img(file_name);
+                        image=img;
+                        cout << "Image loaded successfully!\n";
+                    }
+                } catch (const invalid_argument& e) {
+                    cout << "Error: " << e.what() << '\n';
+                }
+            }
+        }else if(choice == 2){
+            invert(image);
+        }else if(choice == 3){
+            rotate(image);
+        }else if(choice == 4){
+            cout<< "please enter a number from the following choices:\n";
+            cout<<"do you want to 1-save on the same file or 2-change file name\n";
+            int x;
+            cin>>x;
+            if(x==1){
+                image.saveImage(file_name);
+            }else if(x == 2){
+                cout<<"please enter the new file name : ";
+                cin>>newfilename;
+                try {
+                    bool loaded = image.loadNewImage(newfilename);  
+                    if (loaded) {
+                        image.saveImage(newfilename);
+                        cout<<"file saved successfully!\n";                        
+                    }
+                } catch (const invalid_argument& e) {
+                    cout << "Error: " << e.what() << '\n';
+                }
+            }else{
+                cout<<"you enter the wrong number\n";
+            }
+        }else if(choice == 5){
+            break;
+        }else{
+            cout<<"you enter the wrong number\n";
+        }
+    }
+
 
     return 0;
 }
